8279cec351d936ce660d4fd462b3834c6cf50f3e
Stefan Neubig <mail@stefanneubig.com>
2013-05-15 21:07:56 +0200

    Added latest node.js version

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,6 +1,18 @@
 class nodejs {
-    $packages = [ 'nodejs', 'nodejs-dev' ]
-    package { $packages: 
-        ensure => installed,
+
+	exec { 'add_node_repo':
+    	command => '/usr/bin/add-apt-repository ppa:chris-lea/node.js'
     }
+
+    exec { 'update_node_repo':
+        command => '/usr/bin/apt-get update',
+        require => Exec['add_node_repo']
+    }
+
+    $packages = [ 'nodejs' ]
+    package { $packages: 
+        ensure => latest,
+        require => Exec['update_node_repo'],
+    }
+
 } 
