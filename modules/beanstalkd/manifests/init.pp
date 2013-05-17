fc102d9e26e927ff6c253aa8c5a48cc6970f25d3
Ivan Jankovic <ivan@isur.net>
2013-05-17 14:20:20 +0200

    Added support for beanstalkd

--- modules/beanstalkd/manifests/init.pp
+++ modules/beanstalkd/manifests/init.pp
@@ -1,5 +1,31 @@
 class beanstalkd {
-    package { 'beanstalked':
-        ensure => present,
+
+    package { 'beanstalkd':
+        ensure => latest,
+    }
+
+    service { 'beanstalkd':
+        enable => 'true',
+        ensure => 'running',
+        hasstatus => 'true',
+        restart => '/etc/init.d/beanstalkd restart',
+    }
+
+    file { '/etc/default/beanstalkd':
+        owner => 'root',
+        group => 'root',
+        mode  => '644', 
+        ensure => 'present',
+        require => Package['beanstalkd'],
+        source => 'puppet:///modules/beanstalkd/beanstalkd'
+    } 
+
+    file { '/etc/init/beanstalkd.conf':
+		owner => 'root',
+		group => 'root',
+        mode  => '644',
+        ensure => 'present',
+        require => Package['beanstalkd'],
+        source => 'puppet:///modules/beanstalkd/beanstalkd.conf'
     }
 }
