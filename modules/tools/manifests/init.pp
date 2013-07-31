ee14c96af2cbd09e06b3f0d86ca2335c55d29919
Ivan Jankovic <ivan@isur.net>
2013-07-31 20:55:55 +0200

    Added support for compilers, required by some node.js modules

--- modules/tools/manifests/init.pp
+++ modules/tools/manifests/init.pp
@@ -1,5 +1,5 @@
 class tools {
-    $packages = ['python-software-properties', 'curl', 'imagemagick']
+    $packages = ['python-software-properties', 'curl', 'imagemagick', 'build-essential']
     
     package { $packages:
         ensure => installed,
