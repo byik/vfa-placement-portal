bc8f289dded930fd1c39f060eec34dfdd3cfd0de
vagrant <vagrant@devbox>
2013-08-24 07:26:32 +0200

    Added Bootstrap and jQuery. Created demo modal :]

--- www/vfamatching.dev/app/views/layouts/default.blade.php
+++ www/vfamatching.dev/app/views/layouts/default.blade.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="utf-8">
     <title>VFA Placement Portal</title>
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <!-- TODO: Add local fallback for bootstrap -->
+    <!-- TODO: Add local fallback for bootstrap and jQuery -->
 </head>
 <body>
  
