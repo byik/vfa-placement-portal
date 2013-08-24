4eb4a1bdaf279b00b2d4c903795e8b111e54ef02
Scott Lowe <me@scottdlowe.com>
2013-08-24 01:12:14 -0400

    Added bootstrap through CDN and created a default template

---
+++ www/vfamatching.dev/app/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+
+@section('content')
+        <h1>Hello Angular :]</h1>
+@stop
---
+++ www/vfamatching.dev/app/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>VFA Placement Portal</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <!-- TODO: Add local fallback for bootstrap -->
+</head>
+<body>
+ 
+<div class="container">
+	@yield('content')
+</div>
+
+</body>
+</html>
