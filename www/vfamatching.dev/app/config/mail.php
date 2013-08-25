c857dbad8fa5c731fb59fe150a256c96655714d6
Scott Lowe <me@scottdlowe.com>
2013-08-25 00:23:05 -0400

    Configured the email client (Mandrill) and added sample email

--- www/vfamatching.dev/app/config/mail.php
+++ www/vfamatching.dev/app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.mandrillapp.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => "robot@vfamatching.com", 'name' => "The VFA Robot"),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => "mike@ventureforamerica.org",
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => "aTSDRXy_xNTJ9MaF7bFDuw",
 
 	/*
 	|--------------------------------------------------------------------------
--- www/vfamatching.dev/app/routes.php
+++ www/vfamatching.dev/app/routes.php
@@ -13,5 +13,11 @@
 
 Route::get('/', function()
 {
+	$data['name'] = "Scott Lowe";
+
+	Mail::send('emails.welcome', $data, function($message){
+		$message->to('me@scottdlowe.com')->subject("It works!");
+	});
+
 	return View::make('index');
 });
