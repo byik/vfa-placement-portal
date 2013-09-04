4d8dd3d9ba83730e3cf7356ebcd9a6fbfd6049e0
Scott Lowe <me@scottdlowe.com>
2013-09-03 23:17:06 -0400

    Configured database seed for two admin users

--- www/vfamatching.dev/app/database/seeds/UsersTableSeeder.php
+++ www/vfamatching.dev/app/database/seeds/UsersTableSeeder.php
@@ -7,12 +7,18 @@ class UsersTableSeeder extends Seeder {
 		// Uncomment the below to wipe the table clean before populating
 		// DB::table('users')->truncate();
 
-		$users = array(
+		$firstUser = new User();
+		$firstUser->email = "me@scottdlowe.com";
+		$firstUser->password = Hash::make("scottsfakepassword");
+		$firstUser->role = 'Admin';
+		$firstUser->save();
 
-		);
+		$secondUser = new User();
+		$secondUser->email = "lowe0292@gmail.com";
+		$secondUser->password = Hash::make("scottsfakepassword");
+		$secondUser->role = 'Admin';
+		$secondUser->save();
 
-		// Uncomment the below to run the seeder
-		// DB::table('users')->insert($users);
 	}
 
 }
