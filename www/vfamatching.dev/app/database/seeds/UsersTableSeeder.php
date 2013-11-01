bc4a33b76000e2fb1fbcf1c8869c2fea08043d5f
Scott Lowe <me@scottdlowe.com>
2013-10-31 23:36:21 -0400

    Generated new User resource

--- www/vfamatching.dev/app/database/seeds/UsersTableSeeder.php
+++ www/vfamatching.dev/app/database/seeds/UsersTableSeeder.php
@@ -5,18 +5,22 @@ class UsersTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('users')->truncate();
+		DB::table('users')->truncate();
 
 		$firstUser = new User();
 		$firstUser->email = "me@scottdlowe.com";
 		$firstUser->password = Hash::make("scottsfakepassword");
 		$firstUser->role = 'Admin';
+		$firstUser->first_name = "Scott";
+		$firstUser->last_name = "Lowe";
 		$firstUser->save();
 
 		$secondUser = new User();
 		$secondUser->email = "lowe0292@gmail.com";
 		$secondUser->password = Hash::make("scottsfakepassword");
 		$secondUser->role = 'Admin';
+		$firstUser->first_name = "Bruce";
+		$firstUser->last_name = "Wayne";
 		$secondUser->save();
 
 	}
---
+++ www/vfamatching.dev/app/models/User.php
@@ -0,0 +1,7 @@
+<?php
+
+class User extends Eloquent {
+	protected $guarded = array();
+
+	public static $rules = array();
+}
