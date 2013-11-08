0764a8fe49893a46c55389dbae5e9081afd84a29
Scott Lowe <me@scottdlowe.com>
2013-11-07 21:43:08 -0500

    Generated new MediaLink resource

--- www/vfamatching.dev/app/database/seeds/DatabaseSeeder.php
+++ www/vfamatching.dev/app/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder {
 
 		$this->call('UsersTableSeeder');
 		$this->call('CompaniesTableSeeder');
+		$this->call('MedialinksTableSeeder');
 	}
 
 }
--- www/vfamatching.dev/app/routes.php
+++ www/vfamatching.dev/app/routes.php
@@ -19,3 +19,5 @@ Route::get('/', function()
 Route::resource('users', 'UsersController');
 
 Route::resource('companies', 'CompaniesController');
+
+Route::resource('medialinks', 'MedialinksController');
