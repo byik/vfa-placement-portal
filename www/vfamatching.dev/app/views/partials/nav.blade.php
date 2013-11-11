402d61e277f41635720e4defb1f01446a4a48a60
Sean Pennino <pennino.sean@gmail.com>
2013-11-10 18:54:31 -0800

    new vfa logo in navbar

--- www/vfamatching.dev/app/views/partials/nav.blade.php
+++ www/vfamatching.dev/app/views/partials/nav.blade.php
@@ -1,19 +1,19 @@
  <nav class="navbar navbar-default vfa-navbar" role="navigation">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
       <span class="sr-only">Toggle navigation</span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="#">VFA</a>
+    <a class="navbar-brand" href="#"><img src="img/vfa_logo_nav_white.png" alt="Venture for America is the best"></a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
     <ul class="nav navbar-nav navbar-right">
-      <li class="active"><a href="#">Dashboard</a></li>
+      <li class=""><a href="#">Dashboard</a></li>
       <li><a href="#">Profile</a></li>
       <li><a href="#">Opportunities</a></li>
       <li><a href="#">Sign out &raquo;</a></li>
