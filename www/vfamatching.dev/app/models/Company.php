539649ea88ed1076637304319104feebb1fef395
Scott Lowe <me@scottdlowe.com>
2013-11-12 22:53:40 -0500

    Added manytomany relation to Company and MediaLink models

--- www/vfamatching.dev/app/models/Company.php
+++ www/vfamatching.dev/app/models/Company.php
@@ -4,4 +4,9 @@ class Company extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	public function mediaLinks()
+    {
+        return $this->belongsToMany('MediaLink');
+    }
 }
--- www/vfamatching.dev/app/models/MediaLink.php
+++ www/vfamatching.dev/app/models/MediaLink.php
@@ -4,4 +4,9 @@ class MediaLink extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	public function companies()
+    {
+        return $this->belongsToMany('Company');
+    }
 }
