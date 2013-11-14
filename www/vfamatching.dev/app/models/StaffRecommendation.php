13d0f6303df1a52c3c483613c0b3c1e1b12a1ccc
Scott Lowe <me@scottdlowe.com>
2013-11-13 23:50:48 -0500

    Added onetomany relation between StaffRecommendation and Fellow/Opportunity models

--- www/vfamatching.dev/app/models/StaffRecommendation.php
+++ www/vfamatching.dev/app/models/StaffRecommendation.php
@@ -4,4 +4,14 @@ class StaffRecommendation extends Eloquent {
 	protected $guarded = array();
 
 	public static $rules = array();
+
+	public function fellow()
+	{
+		$this->belongsTo('Fellow');
+	}
+
+	public function company()
+	{
+		$this->belongsTo('Company');
+	}
 }
