44b230a984d4d71872a123fcd751347ea1f3fc07
Acefox17 <timcrowe@me.com>
2013-11-15 20:18:10 -0600

    added rules to company model

--- www/vfamatching.dev/app/models/Company.php
+++ www/vfamatching.dev/app/models/Company.php
@@ -3,7 +3,32 @@
 class Company extends Eloquent {
 	protected $guarded = array();
 
-	public static $rules = array();
+	public static $createRules = array(
+        'name'=>'max:280|unique';
+        'city'=>'max:280';
+        'url'=>'url';
+        'tagline'=>'max:140';
+        'visionAnswer'=>'max:280';
+        'needsAnswer'=>'max:280';
+        'teamAnswer'=>'max:280';
+        'employees'=>'integer';
+        'yearFounded'=>'integer';
+        'twitterHandle'=>'max:15';
+        );
+
+    public static $updateRules = array(
+        'name'=>'max:280';
+        'city'=>'max:280';
+        'url'=>'url';
+        'tagline'=>'max:140';
+        'visionAnswer'=>'max:280';
+        'needsAnswer'=>'max:280';
+        'teamAnswer'=>'max:280';
+        'employees'=>'integer';
+        'yearFounded'=>'integer';
+        'twitterHandle'=>'max:15';
+        );
+        );
 
 	public function mediaLinks()
     {
