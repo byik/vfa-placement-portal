856c50a1d205d85f8e4ed92503ee01a6ab73f16d
Sean Pennino <pennino.sean@gmail.com>
2013-11-21 20:18:03 -0800

    updates to fellow dash: relationships pie charts & collapsing events

--- www/vfamatching.dev/app/views/index.blade.php
+++ www/vfamatching.dev/app/views/index.blade.php
@@ -5,34 +5,111 @@ Dashboard
 @stop
 
 @section('content')
-<div class="fellow-dash">
-	<div class="container">
-		<h2><small>Upcoming Events<small></h2>
-		<ul>
-			<li><span class="glyphicon glyphicon-phone"></span><span class="red"> March 23, 6:00pm</span>   Phone interview with Chalkfly</li>
-			<li><span class="glyphicon glyphicon-user"></span><span class="red"> April 8, 2:00pm</span>   In person interview with Teespring</li>
-			<li><span class="glyphicon glyphicon-user"></span><span class="red"> April 12, 3:00pm</span>  In person interview with Swipely</li>
-			<li><span class="glyphicon glyphicon-thumbs-up"></span><span class="red"> April 17, 5:00pm</span>  VCharge offer acceptance deadline</li>
-		</ul>
-		<h2><small>Relationships</small></h2>
-	</div>
-</div>
+
+@if( Auth::check() )
+
+	@if( Auth::user()->role == "Admin" )
+
+	@elseif( Auth::user()->role == "Fellow")
+		<div class="fellow-dash">
+			<div class="container">
+				<div class="upcoming-events">
+						<h2><small>Upcoming Events<small></h2>
+						<div class="row">
+							<div class="col-md-1"><span class="glyphicon glyphicon-phone"></span></div>
+							<div class="col-md-2"><span class="red"> March 23, 6:00pm</span></div>
+							<div class="col-md-9">Phone interview with Chalkfly</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1"><span class="glyphicon glyphicon-user"></span></div>
+							<div class="col-md-2"><span class="red"> April 8, 2:00pm</span> </div>
+							<div class="col-md-9">In person interview with Teespring</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1"><span class="glyphicon glyphicon-user"></span></div>
+							<div class="col-md-2"><span class="red"> April 12, 3:00pm</span></div>
+							<div class="col-md-9">In person interview with Swipely</div>
+						</div>
+						<div class="row">
+							<div class="col-md-1"><span class="glyphicon glyphicon-thumbs-up"></span></div>
+							<div class="col-md-2"><span class="red"> April 17, 5:00pm</span></div>
+							<div class="col-md-9">VCharge offer acceptance deadline</div>
+						</div>
+				</div>
+				<div class="relationships">
+					<h2><small>Relationships</small></h2>
+					<div class="row">
+						<div class="col-md-4">
+							<canvas id="myChart1" width="100" height="100"></canvas>
+							<script type="text/javascript">
+								var data = [
+								{
+									value: 30,
+									color:"#ec5a41"
+								},
+								{
+									value: 80,
+									color:"#dedede"
+								}		
+							]
+
+								//Get context with jQuery - using jQuery's .get() method.
+								var ctx = $("#myChart1").get(0).getContext("2d");
+
+								new Chart(ctx).Pie(data);
+							</script>		
+						</div>
+						<div class="col-md-4">
+							<canvas id="myChart2" width="100" height="100"></canvas>
+							<script type="text/javascript">
+								var data = [
+								{
+									value: 30,
+									color:"#ec5a41"
+								},
+								{
+									value: 40,
+									color:"#dedede"
+								}		
+							]
+
+								//Get context with jQuery - using jQuery's .get() method.
+								var ctx = $("#myChart2").get(0).getContext("2d");
+
+								new Chart(ctx).Pie(data);
+							</script>		
+						</div>
+						<div class="col-md-4">
+							<canvas id="myChart3" width="100" height="100"></canvas>
+							<script type="text/javascript">
+								var data = [
+								{
+									value: 30,
+									color:"#ec5a41"
+								},
+								{
+									value: 100,
+									color:"#dedede"
+								}		
+							]
+
+								//Get context with jQuery - using jQuery's .get() method.
+								var ctx = $("#myChart3").get(0).getContext("2d");
+
+								new Chart(ctx).Pie(data);
+							</script>		
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
-<canvas id="myChart" width="400" height="400"></canvas>
+	@elseif( Auth::user()->role == "Hiring Manager")
 
-<script type="text/javascript">
-	var data = [
-	{
-		value: 30,
-		color:"#F38630"
-	}		
-]
-
-	//Get context with jQuery - using jQuery's .get() method.
-	var ctx = $("#myChart").get(0).getContext("2d");
-	//This will get the first returned node in the jQuery collection.
-	new Chart(ctx).Pie(data);
-</script>
+    @else
+  <!-- We've got problems -->
+	@endif
+@endif
 
 @stop
