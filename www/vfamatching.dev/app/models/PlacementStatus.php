54ad31d0d161346467219f5fdae335735703ab36
Scott Lowe <me@scottdlowe.com>
2013-11-21 22:40:36 -0500

    Updated PlacementStatus model to extend BaseModel and added validation rules

--- www/vfamatching.dev/app/models/PlacementStatus.php
+++ www/vfamatching.dev/app/models/PlacementStatus.php
@@ -1,30 +1,38 @@
 <?php
 
-class PlacementStatus extends Eloquent {
-	protected $guarded = array();
+class PlacementStatus extends BaseModel {
+    protected function rules()
+    {
+        return array(
+            'status'=> 'required|in:Introduced,Contacted,Phone Interview Pending,Phone Interview Complete,On-site Interview Pending,On-site Interview Complete,Offer Extended,Offer Accepted,Offer Rejected',
+            'eventDate'=>'date',
+            'score'=>'required|in:Admin,Fellow,Hiring Manager',
+            'message'=>'required|max:280');
+    }
 
-	public static $rules = array();
+    protected $guarded = array();
 
-	public function fellow()
+    public function fellow()
     {
         return $this->belongsTo('Fellow');
     }
 
     public function opportunity()
     {
-    	return $this->belongsTo('Opportunity');
+        return $this->belongsTo('Opportunity');
     }
 
     public static function statuses()
     {
-        return array("Introduced",
-            "Contacted",
-            "Phone Interview Pending",
-            "Phone Interview Complete",
-            "On-site Interview Pending",
-            "On-site Interview Complete",
-            "Offer Extended",
-            "Offer Accepted",
-            "Offer Rejected");
+        return array(
+            'Introduced',
+            'Contacted',
+            'Phone Interview Pending',
+            'Phone Interview Complete',
+            'On-site Interview Pending',
+            'On-site Interview Complete',
+            'Offer Extended',
+            'Offer Accepted',
+            'Offer Rejected');
     }
 }
