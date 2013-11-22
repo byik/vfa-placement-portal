6cdcb3d17c69afb61ed30b345c36b1c3e093f7ee
Sean Pennino <pennino.sean@gmail.com>
2013-11-21 19:01:12 -0800

    added pie chart example on dashboard

--- www/vfamatching.dev/app/views/index.blade.php
+++ www/vfamatching.dev/app/views/index.blade.php
@@ -17,5 +17,22 @@ Dashboard
 		<h2><small>Relationships</small></h2>
 	</div>
 </div>
-@stop
 
+
+<canvas id="myChart" width="400" height="400"></canvas>
+
+<script type="text/javascript">
+	var data = [
+	{
+		value: 30,
+		color:"#F38630"
+	}		
+]
+
+	//Get context with jQuery - using jQuery's .get() method.
+	var ctx = $("#myChart").get(0).getContext("2d");
+	//This will get the first returned node in the jQuery collection.
+	new Chart(ctx).Pie(data);
+</script>
+
+@stop
