faae4d48308b7008eb3a0914bf087673823cb661
Sean Pennino <pennino.sean@gmail.com>
2013-11-22 18:29:30 -0800

    updates to fellow dash format

--- www/vfamatching.dev/app/views/index.blade.php
+++ www/vfamatching.dev/app/views/index.blade.php
@@ -16,24 +16,20 @@ Dashboard
 				<div class="upcoming-events">
 						<h2><small>UPCOMING EVENTS<small></h2>
 						<div class="row">
-							<div class=""></div>
 							<div class="col-md-3"><span class="glyphicon glyphicon-phone"></span><span class="red date"> March 23, 6:00pm</span></div>
-							<div class="col-md-9">Phone interview with Chalkfly</div>
+							<div class="col-md-9"><span>Phone interview with Chalkfly</span></div>
 						</div>
 						<div class="row">
-							<div class=""></div>
 							<div class="col-md-3"><span class="glyphicon glyphicon-user"></span><span class="red date"> April 8, 2:00pm</span> </div>
-							<div class="col-md-9">In person interview with Teespring</div>
+							<div class="col-md-9"><span>In person interview with Teespring</span></div>
 						</div>
 						<div class="row">
-							<div class=""></div>
 							<div class="col-md-3"><span class="glyphicon glyphicon-user"></span><span class="red date"> April 12, 3:00pm</span></div>
-							<div class="col-md-9">In person interview with Swipely</div>
+							<div class="col-md-9"><span>In person interview with Swipely</span></div>
 						</div>
 						<div class="row">
-							<div class=""></div>
 							<div class="col-md-3"><span class="glyphicon glyphicon-thumbs-up"></span><span class="red date"> April 17, 5:00pm</span></div>
-							<div class="col-md-9">VCharge offer acceptance deadline</div>
+							<div class="col-md-9"><span>VCharge offer acceptance deadline</span></div>
 						</div>
 				</div>
 				<div class="relationships">
