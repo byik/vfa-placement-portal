7f4e74a11134e4a3aca2d6217e5ecfc1729d0f71
Scott Lowe <me@scottdlowe.com>
2013-11-28 17:51:59 -0500

    Refactored relationship pie chart into subview. Nested in index

--- www/vfamatching.dev/app/views/index.blade.php
+++ www/vfamatching.dev/app/views/index.blade.php
@@ -34,10 +34,18 @@ Dashboard
 				</div>
 				<div class="relationships">
 					<h2><small>RELATIONSHIPS</small></h2>
-						@foreach($relationships as $relationship)
-							@include('partials.indexes.relationship', array('relationship' => $relationship))
-						@endforeach
-					</div>
+                        <div class="row">
+    						@foreach($relationships as $relationship)
+                                <?php 
+                                    $pieChart = new PieChart;
+                                    $pieChart->id = $relationship->id;
+                                    $pieChart->width = 100;
+                                    $pieChart->height = 100;
+                                    $pieChart->percent = $relationship->percent();
+                                ?>
+    							@include('partials.indexes.relationship', array('relationship' => $relationship))->nest('pieChart','partials.charts.pie-percent', array('pieChart' => $pieChart))
+    						@endforeach
+					    </div>
 				</div>
 			</div>
 		</div>
--- www/vfamatching.dev/app/views/partials/indexes/relationship.blade.php
+++ www/vfamatching.dev/app/views/partials/indexes/relationship.blade.php
@@ -1,25 +1,7 @@
-<div class="row">
-	<div class="col-md-4">
-		<div class="row">
-			<div class="col-md-1">
-				<canvas id="myChart{{ $relationship->id }}" width="100" height="100"></canvas>
-				<script type="text/javascript">
-					var data = [
-					{
-						value: {{ $relationship->percent() }},
-						color:"#ec5a41"
-					},
-					{
-						value: {{ 1 - $relationship->percent() }},
-						color:"#dedede"
-					}		
-				]
-
-					//Get context with jQuery - using jQuery's .get() method.
-					var ctx = $("#myChart{{ $relationship->id }}").get(0).getContext("2d");
-
-					new Chart(ctx).Pie(data);
-				</script>
-			</div>
-		</div>		
-	</div>
+<div class="col-md-4">
+	<div class="row">
+		<div class="col-md-1">
+            {{ $pieChart }}
+		</div>
+	</div>		
+</div>
