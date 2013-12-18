2a9d150ed08ebc335fcf839135eb90f8d10a199f
Sean Pennino <pennino.sean@gmail.com>
2013-12-17 23:34:36 -0800

    added container divs to alerts

--- www/vfamatching.dev/app/views/partials/alerts/error.blade.php
+++ www/vfamatching.dev/app/views/partials/alerts/error.blade.php
@@ -1,8 +1,10 @@
 <!-- @param $error the String message to display -->
 
-<div class="row alert alert-danger alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert">
-		<span class="glyphicon glyphicon-remove"></span>
-	</button>
-	{{ $error }}
+<div class="container">
+	<div class="row alert alert-danger alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert">
+			<span class="glyphicon glyphicon-remove"></span>
+		</button>
+		{{ $error }}
+	</div>
 </div>
--- www/vfamatching.dev/app/views/partials/alerts/notice.blade.php
+++ www/vfamatching.dev/app/views/partials/alerts/notice.blade.php
@@ -1,8 +1,10 @@
 <!-- @param $notice the String message to display -->
 
-<div class="row alert alert-info alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert">
-		<span class="glyphicon glyphicon-remove"></span>
-	</button>
-	{{ $notice }}
+<div class="container">
+	<div class="row alert alert-info alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert">
+			<span class="glyphicon glyphicon-remove"></span>
+		</button>
+		{{ $notice }}
+	</div>
 </div>
