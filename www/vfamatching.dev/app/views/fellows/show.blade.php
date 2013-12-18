85dede34bdae5d36d416d998800cb3274cec6bfa
Sean Pennino <pennino.sean@gmail.com>
2013-12-17 23:47:41 -0800

    put fellow profile in container div

--- www/vfamatching.dev/app/views/fellows/show.blade.php
+++ www/vfamatching.dev/app/views/fellows/show.blade.php
@@ -5,11 +5,13 @@
 @stop
 
 @section('content')
-    @if(Auth::user()->id == $fellow->user->id)
+<div class="container">
+	@if(Auth::user()->id == $fellow->user->id)
         <a href="{{ URL::route('fellows.edit', $fellow->id) }}">Edit your profile &raquo;</a>
     @endif
     <h1>{{ $fellow->user->firstName . ' ' . $fellow->user->lastName }}</h1>
     @if(!empty($fellow->displayPicturePath))
     	<img src="{{ $fellow->displayPicturePath }}" class="img-responsive" alt="Responsive image">
     @endif
+</div>
 @stop
