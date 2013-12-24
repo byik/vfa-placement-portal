b924725c6aacb8935b62b4d5dec22fc6be48f49e
Scott Lowe <me@scottdlowe.com>
2013-12-23 22:17:52 -0500

    [migration] Adding teaser column to opportunities table

---
+++ www/vfamatching.dev/app/database/migrations/2013_12_24_025732_add_teaser_to_opportunities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddTeaserToOpportunitiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('opportunities', function(Blueprint $table) {
+			$table->string('teaser');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('opportunities', function(Blueprint $table) {
+			$table->dropColumn('teaser');
+		});
+	}
+
+}
