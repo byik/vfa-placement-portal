d88a1e2d3a4856192e3af6626698227f060f59a6
Scott Lowe <me@scottdlowe.com>
2013-12-29 18:06:53 -0500

    Add button now on Users index for Admins

--- www/vfamatching.dev/app/views/users/index.blade.php
+++ www/vfamatching.dev/app/views/users/index.blade.php
@@ -5,5 +5,8 @@
 @stop
 
 @section('content')
+	@if(Auth::user()->role == "Admin")
+		@include('partials.components.add-button', array('url'=>'/users/create', 'name' => 'User'))
+	@endif
     @include('partials.list', array('listItems' => $users, 'search' => $search, 'url' => URL::route('users.index'), 'pills' => $pills, 'indexView' => 'partials.indexes.user', 'type' => 'user', 'total' => $total))
 @stop
