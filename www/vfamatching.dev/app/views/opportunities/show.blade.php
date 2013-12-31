9d45a6b1e23c793f9a4eca5ae69092bdce146b69
Sean Pennino <pennino.sean@gmail.com>
2013-12-31 14:58:02 -0800

    updates to opportunity profile

--- www/vfamatching.dev/app/views/opportunities/show.blade.php
+++ www/vfamatching.dev/app/views/opportunities/show.blade.php
@@ -8,30 +8,30 @@
 @section('content')
     <div class="container">
         <div class="row">
+        	<div class="col-md-4">
+                <h2>@include('partials.links.company', array('company' => $opportunity->company))</h2>
+            </div>
             <div class="col-md-8">
                 @include('partials.components.tags', array('tags' => $opportunity->opportunityTags))
-            </div>
-            <div class="col-md-4">
-                <h2>@include('partials.links.company', array('company' => $opportunity->company))</h2>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-            	<h3><strong>Opportunity Description</strong></h3>
+            	<h4><strong>Opportunity Description</strong></h4>
             	<p>{{ Parser::linkUrlsInText($opportunity->description) }}</p>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
-            	<h4><em>What will the fellow's major daily responsibilities be?</em></h4>
+            	<h4><strong><em>What will the fellow's major daily responsibilities be?</em></strong></h4>
             	<p>{{ $opportunity->responsibilitiesAnswer	}}</p>
             </div>
             <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
-            	<h4><em>What skills are required for this role?</em></h4>
+            	<h4><strong><em>What skills are required for this role?</em></strong></h4>
             	<p>{{ $opportunity->skillsAnswer }}</p>
             </div>
             <div class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0">
-                <h4><em>How will the fellow likely develop in this role?</em></h4>
+                <h4><strong><em>How will the fellow likely develop in this role?</em></strong></h4>
                 <p>{{ $opportunity->developmentAnswer }}</p>
             </div>
         </div>
