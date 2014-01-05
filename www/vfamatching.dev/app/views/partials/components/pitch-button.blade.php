05fcede37054ae679452a692076caacd2f0ee485
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 19:39:51 -0800

    changed pitch button to green

--- www/vfamatching.dev/app/views/partials/components/pitch-button.blade.php
+++ www/vfamatching.dev/app/views/partials/components/pitch-button.blade.php
@@ -4,6 +4,6 @@
     @elseif(Pitch::hasPitch(Auth::user()->profile, $opportunity))
         <strong>Pitch Status: </strong>{{ Pitch::getPitch(Auth::user()->profile, $opportunity)->status }}
     @else
-        <a data-toggle="modal" href="#pitch-modal-{{ $opportunity->id }}" class="btn btn-primary modal-btn form-control"><i class="fa fa-comment"></i> Pitch</a>
+        <a data-toggle="modal" href="#pitch-modal-{{ $opportunity->id }}" class="btn btn-success modal-btn form-control"><i class="fa fa-comment"></i> Pitch</a>
     @endif
 @endif
