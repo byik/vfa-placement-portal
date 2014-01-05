5f36dc7dd2ff985aa227e4f2838a1f76a73dba28
Sean Pennino <pennino.sean@gmail.com>
2014-01-05 02:25:44 -0800

    styling edits to hiring manager dashboard

--- www/vfamatching.dev/app/views/partials/dashboards/hiringManager.blade.php
+++ www/vfamatching.dev/app/views/partials/dashboards/hiringManager.blade.php
@@ -2,14 +2,14 @@
 	<div class="upcoming-events">
 		<div class="row">
 			<div class="col-md-12">
-				<h3>Upcoming Events</h3>
+				<h2><small>UPCOMING EVENTS</small></h2>
 			</div>
 			<div class="col-md-3"><span class="red date"> 2 months from now </span></div>
 	        <div class="col-md-9"><span><i class="fa fa-mobile"></i>Phone Interview with <a href="">John Doe</a></span></div>
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<h3>Opportunities</h3>
+				<h2><small>OPPORTUNITIES</small></h2>
 			</div>
 		</div>
 	</div>
