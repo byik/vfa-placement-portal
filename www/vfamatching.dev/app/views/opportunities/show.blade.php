5af98bc6c0b469f14473b52cf83bba645ed177de
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 18:54:45 -0800

    added pitch button to opportunity profile

--- www/vfamatching.dev/app/views/opportunities/show.blade.php
+++ www/vfamatching.dev/app/views/opportunities/show.blade.php
@@ -8,9 +8,10 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-9">
                 @include('partials.components.tags', array('tags' => $opportunity->opportunityTags))
             </div>
+            <div class="col-md-3">@include('partials.components.pitch-button')</div>
         </div>
         <div class="row">
             <div class="col-md-12">
