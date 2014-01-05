8f0d782862ff669c098a8d7d0bbe8f9e6abd221b
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 17:09:53 -0800

    update to upcoming events partial

--- www/vfamatching.dev/app/views/partials/indexes/upcoming-events.blade.php
+++ www/vfamatching.dev/app/views/partials/indexes/upcoming-events.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
     @if($placementStatus->status == PlacementStatus::statuses()[2])
-    	<div class="col-md-3"><span class="red date"><i class="fa fa-mobile"></i> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }}</span></div>
-        <div class="col-md-9"><span>Phone Interview</span></div>
+    	<div class="col-md-3"><span class="red date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }} </span></div>
+        <div class="col-md-9"><span><i class="fa fa-mobile"></i>Phone Interview</span></div>
     @elseif($placementStatus->status == PlacementStatus::statuses()[4])
-        <div class="col-md-3"><span class="red date"><i class="fa fa-user"></i> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }}</span></div>
-        <div class="col-md-9"><span>On-site Interview</span></div>
+        <div class="col-md-3"><span class="red date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }}</span></div>
+        <div class="col-md-9"><span><i class="fa fa-user"></i>On-site Interview</span></div>
     @elseif($placementStatus->status == PlacementStatus::statuses()[6])
-        <div class="col-md-3"><span class="red date"><i class="fa fa-thumbs-up"></i> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }}</span></div>
-        <div class="col-md-9"><span>Offer Acceptance Deadline</span></div>
+        <div class="col-md-3"><span class="red date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans(); }}</span></div>
+        <div class="col-md-9"><span><i class="fa fa-thumbs-up"></i>Offer Acceptance Deadline</span></div>
     @endif
 </div>
