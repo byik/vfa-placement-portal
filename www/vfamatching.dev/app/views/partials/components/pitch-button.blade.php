00d4473a5b7b3d13807fc812476bfedb225a6d2b
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 19:43:15 -0800

    added modal to refactored pitch button

--- www/vfamatching.dev/app/views/partials/components/pitch-button.blade.php
+++ www/vfamatching.dev/app/views/partials/components/pitch-button.blade.php
@@ -6,4 +6,24 @@
     @else
         <a data-toggle="modal" href="#pitch-modal-{{ $opportunity->id }}" class="btn btn-success modal-btn form-control"><i class="fa fa-comment"></i> Pitch</a>
     @endif
+	<!-- Modal -->
+	<div class="modal" id="pitch-modal-{{ $opportunity->id }}">
+	    <div class="modal-dialog">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <a href="#" class="btn close btn-default" data-dismiss="modal">&times;</a>
+	                <h4 class="modal-title">Pitch for the {{ $opportunity->title }} Opportunity at {{ $opportunity->company->name }}</h4>
+	            </div>
+	            <div class="modal-body">
+	                @if(Auth::user()->role == "Fellow")
+	                    @include('partials.forms.pitch', array('fellow_id' => Auth::user()->profile->id, 'opportunity_id' => $opportunity->id))
+	                @endif
+	            </div>
+	            <div class="modal-footer">
+	                <a href="" class="btn btn-default" data-dismiss="modal">Cancel</a>
+	                <a href="" class="btn btn-primary pitch-submit">Submit</a>
+	            </div>
+	        </div><!-- /.modal-content -->
+	    </div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
 @endif
--- www/vfamatching.dev/app/views/partials/indexes/opportunity.blade.php
+++ www/vfamatching.dev/app/views/partials/indexes/opportunity.blade.php
@@ -34,28 +34,7 @@
         </div>
     </div>
 </div>
-@if(Auth::user()->role == "Fellow")
-<!-- Modal -->
-<div class="modal" id="pitch-modal-{{ $opportunity->id }}">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <a href="#" class="btn close btn-default" data-dismiss="modal">&times;</a>
-                <h4 class="modal-title">Pitch for the {{ $opportunity->title }} Opportunity at {{ $opportunity->company->name }}</h4>
-            </div>
-            <div class="modal-body">
-                @if(Auth::user()->role == "Fellow")
-                    @include('partials.forms.pitch', array('fellow_id' => Auth::user()->profile->id, 'opportunity_id' => $opportunity->id))
-                @endif
-            </div>
-            <div class="modal-footer">
-                <a href="" class="btn btn-default" data-dismiss="modal">Cancel</a>
-                <a href="" class="btn btn-primary pitch-submit">Submit</a>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-@endif
+
 <script type="text/javascript">
     $('.pitch-submit').unbind().click(function(e){
         $(this).parent().parent().find('.pitch-form').submit();
