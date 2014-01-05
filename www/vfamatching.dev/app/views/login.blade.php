67267997621b459943c3818062cf87052244cdce
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 23:32:30 -0800

    added container to login page

--- www/vfamatching.dev/app/views/login.blade.php
+++ www/vfamatching.dev/app/views/login.blade.php
@@ -6,25 +6,27 @@ Placement Portal Login
 
 @section('content')
 
-<div id="login" class="well col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-    {{ Form::open(array('route' => 'login')) }}
-        <fieldset>
-            <!-- email field -->
-            <div class="form-group">
-                {{ Form::label('email', 'Email') }}<br/>
-                {{ Form::text('email', Input::old('email'), array('class'=>'form-control required requires-email')) }}
-            </div>
+<div class="container">
+    <div id="login" class="well col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+        {{ Form::open(array('route' => 'login')) }}
+            <fieldset>
+                <!-- email field -->
+                <div class="form-group">
+                    {{ Form::label('email', 'Email') }}<br/>
+                    {{ Form::text('email', Input::old('email'), array('class'=>'form-control required requires-email')) }}
+                </div>
 
-            <!-- password field -->
-            <div class="form-group">
-                {{ Form::label('password', 'Password') }}<br/>
-                {{ Form::password('password', array('class'=>'form-control required')) }}
-            </div>
+                <!-- password field -->
+                <div class="form-group">
+                    {{ Form::label('password', 'Password') }}<br/>
+                    {{ Form::password('password', array('class'=>'form-control required')) }}
+                </div>
 
-            <!-- submit button -->
-            <p>{{ Form::submit('Login',  array('class'=>'btn btn-primary form-control')) }}</p>
-        </fieldset>
-    {{ Form::close() }}
+                <!-- submit button -->
+                <p>{{ Form::submit('Login',  array('class'=>'btn btn-primary form-control')) }}</p>
+            </fieldset>
+        {{ Form::close() }}
+    </div>
 </div>
 
 @stop
