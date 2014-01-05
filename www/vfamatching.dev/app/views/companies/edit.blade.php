459704fe554bea0459f8b6150cc64388e9d979b4
Sean Pennino <pennino.sean@gmail.com>
2014-01-04 22:13:54 -0800

    begun form for companies to edit info

--- www/vfamatching.dev/app/views/companies/edit.blade.php
+++ www/vfamatching.dev/app/views/companies/edit.blade.php
@@ -1,1 +1,15 @@
-edit.blade
+@extends('layouts.default')
+
+@section('header')
+    Company Profile
+@stop
+
+@section('content')
+    	<div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    
+                </div>
+            </div>   
+        </div>
+@stop
