5d169bb2b369e51f3056076df632b97f1fa32056
Scott Lowe <me@scottdlowe.com>
2014-01-15 00:23:28 -0500

    Optimising default layout for production

--- www/vfamatching.dev/app/views/layouts/default.blade.php
+++ www/vfamatching.dev/app/views/layouts/default.blade.php
@@ -3,27 +3,23 @@
     <head>
         <meta charset="utf-8">
         <title>VFA Placement Portal</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- TODO: Add local fallback for bootstrap and jQuery -->
-        <!-- jQuery -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-        <!-- Bootstrap -->
+        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{ URL::to('css/bootstrap.css') }}">
         <!-- Fontawesome -->
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-        <script src="{{ URL::to('js/bootstrap.js') }}"></script>
-        <!-- Datepicker -->
+        <!-- Datepicker CSS -->
         <link rel="stylesheet" href="{{ URL::to('css/datepicker.css') }}">
-        <script src="{{ URL::to('js/bootstrap-datepicker.js') }}"></script>
-        <!-- Chart.js -->
-        <script src="{{ URL::to('js/Chart.js') }}"></script>
-        <!-- Noty -->
-        <script type="text/javascript" src="{{ URL::to('js/noty/packaged/jquery.noty.packaged.min.js') }}"></script>
-        <!-- character-limit.js -->
-        <script src="{{ URL::to('js/character-limit.js') }}"></script>
-        <!-- required-fields.js -->
-        <script src="{{ URL::to('js/realtime-input-feedback.js') }}"></script>
         <!-- Our custom css -->
         <link rel="stylesheet" href="{{ URL::to('css/style.css') }}">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body> 
         @include('partials.nav')
@@ -50,6 +46,21 @@
             @yield('content')
         </div>
         @include('partials.footer')
+        
+        <!-- jQuery -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="{{ URL::to('js/bootstrap.js') }}"></script>
+        <!-- Datepicker JS -->
+        <script src="{{ URL::to('js/bootstrap-datepicker.js') }}"></script>
+        <!-- Chart.js -->
+        <script src="{{ URL::to('js/Chart.js') }}"></script>
+        <!-- Noty -->
+        <script type="text/javascript" src="{{ URL::to('js/noty/packaged/jquery.noty.packaged.min.js') }}"></script>
+        <!-- character-limit.js -->
+        <script src="{{ URL::to('js/character-limit.js') }}"></script>
+        <!-- required-fields.js -->
+        <script src="{{ URL::to('js/realtime-input-feedback.js') }}"></script>
     </body>
 </html>
 
