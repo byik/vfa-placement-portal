2d378165ba29571219114c3d24f59d5f6fc36c6f
root <root@match.ventureforamerica.org>
2014-01-18 16:02:52 -0500

    Camel-casing controller names in routes.php

--- www/vfamatching.dev/app/routes.php
+++ www/vfamatching.dev/app/routes.php
@@ -20,28 +20,28 @@ Route::group(array('before' => 'auth'), function()
     /* These routes are available to users without profiles */
     Route::get('fellows/create', array('uses' => 'FellowsController@create'));
     Route::post('fellows', array('uses' => 'FellowsController@store'));
-    Route::get('hiringmanagers/create', array('uses' => 'HiringmanagersController@create'));
-    Route::post('hiringmanagers', array('uses' => 'HiringmanagersController@store'));
-    Route::get('hiringmanagers/{id}/edit', array('uses' => 'HiringmanagersController@edit'));
-    Route::put('hiringmanagers/{id}', array('uses' => 'HiringmanagersController@update'));
+    Route::get('hiringmanagers/create', array('uses' => 'HiringManagersController@create'));
+    Route::post('hiringmanagers', array('uses' => 'HiringManagersController@store'));
+    Route::get('hiringmanagers/{id}/edit', array('uses' => 'HiringManagersController@edit'));
+    Route::put('hiringmanagers/{id}', array('uses' => 'HiringManagersController@update'));
     Route::get('companies/{id}/edit', array('uses' => 'CompaniesController@edit'));
     Route::group(array('before' => 'profile'), function() /* Requires a profile to be created before using the site */
     {
         Route::get('/', array('as' => 'dashboard', 'uses' => 'UsersController@dashboard'));
         Route::resource('users', 'UsersController');
         Route::resource('companies', 'CompaniesController');
-        Route::resource('medialinks', 'MedialinksController');
+        Route::resource('medialinks', 'MediaLinksController');
         Route::resource('fellows', 'FellowsController');
         Route::resource('opportunities', 'OpportunitiesController');
-        Route::resource('hiringmanagers', 'HiringmanagersController');
+        Route::resource('hiringmanagers', 'HiringManagersController');
         Route::resource('admins', 'AdminsController');
-        Route::resource('placementstatuses', 'PlacementstatusesController');
-        Route::resource('adminnotes', 'AdminnotesController');
-        Route::resource('fellownotes', 'FellownotesController');
+        Route::resource('placementstatuses', 'PlacementStatusesController');
+        Route::resource('adminnotes', 'AdminNotesController');
+        Route::resource('fellownotes', 'FellowNotesController');
         Route::resource('pitches', 'PitchesController');
-        Route::resource('fellowskills', 'FellowskillsController');
-        Route::resource('opportunitytags', 'OpportunitytagsController');
-        Route::resource('staffrecommendations', 'StaffrecommendationsController');
+        Route::resource('fellowskills', 'FellowSkillsController');
+        Route::resource('opportunitytags', 'OpportunityTagsController');
+        Route::resource('staffrecommendations', 'StaffRecommendationsController');
         Route::put('opportunities/{id}/publish', 'OpportunitiesController@publish');
         Route::put('opportunities/{id}/unpublish', 'OpportunitiesController@unpublish');
         Route::group(array('before' => 'admin'), function() /* Requires the user to be an admin */
