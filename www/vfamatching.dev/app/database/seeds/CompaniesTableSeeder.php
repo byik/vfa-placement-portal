e8e9320d9d2853568c09d09d5f770922ae8ac226
Scott Lowe <me@scottdlowe.com>
2014-01-18 16:25:01 -0500

    Adding Company bios to company seeder

--- www/vfamatching.dev/app/database/seeds/CompaniesTableSeeder.php
+++ www/vfamatching.dev/app/database/seeds/CompaniesTableSeeder.php
@@ -11,6 +11,9 @@ class CompaniesTableSeeder extends Seeder {
         $firstCompany->name = "Chalkfly";
         $firstCompany->city = "Detroit";
         $firstCompany->url = "http://www.chalkfly.com";
+        $firstCompany->bio = "Chalkfly is an ecommerce startup in downtown Detroit. We sell school and office supplies online so we can give 5% of every sale back to teachers to buy school supplies for their students, but Chalkfly is so much more than that.
+We're a team of misfits that work hard and play hard. We're constantly reinventing ourselves, and we're driven to make a dent in the world. At Chalkfly, feedback is king and no two days are the same.
+So do you want to be a corporate cog when you grow up, or do you want to join us and build something that matters?";
         $firstCompany->twitterPitch = "Supply & Delight";
         $firstCompany->visionAnswer = "We want to be the Zappos of office supplies.";
         $firstCompany->needsAnswer = "We need eCommerce experts.";
@@ -26,6 +29,8 @@ class CompaniesTableSeeder extends Seeder {
         $secondCompany->city = "Providence";
         $secondCompany->url = "http://www.teespring.com";
         $secondCompany->twitterPitch = "Awesome Tshirts";
+        $secondCompany->bio = "Teespring allows you to create & sell custom tees with zero upfront costs, and zero risk.
+Create & sell t-shirts online the easy way. No paying thousands of dollars upfront, no guessing how many shirts or what sizes you'll need, and no passing out t-shirts one by one and chasing people down for cash.";
         $secondCompany->visionAnswer = "We want to sell all the world's t-shirt's.";
         $secondCompany->needsAnswer = "We need more people like Sean.";
         $secondCompany->teamAnswer = "Teespring is a fun team.";
