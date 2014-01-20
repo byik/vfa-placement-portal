c9813700b6bf963a30dde0cdc2d997d6619146b3
Scott Lowe <me@scottdlowe.com>
2014-01-19 21:08:14 -0500

    Adding default layout to create opportunity view

--- www/vfamatching.dev/app/views/opportunities/create.blade.php
+++ www/vfamatching.dev/app/views/opportunities/create.blade.php
@@ -1,1 +1,15 @@
-create.blade
+@extends('layouts.default')
+
+@section('header')
+Create New Opportunity
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            @include('partials.forms.opportunity')
+        </div>
+    </div>
+</div>
+@stop
