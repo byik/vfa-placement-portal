41e2b9ee686caeb4c7a7f6d01f9d2531b2bb96fd
Scott Lowe <me@scottdlowe.com>
2014-02-20 21:04:55 -0500

    [bugfix] Displaying upcoming events for Hiring Managers accurately

--- www/vfamatching.dev/app/views/partials/indexes/upcoming-events.blade.php
+++ www/vfamatching.dev/app/views/partials/indexes/upcoming-events.blade.php
@@ -1,12 +1,25 @@
 <div class="row">
-    @if($placementStatus->status == PlacementStatus::statuses()[2])
-    	<div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
-        <div class="col-md-9"><span><i class="fa fa-mobile"></i>Phone Interview with <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
-    @elseif($placementStatus->status == PlacementStatus::statuses()[4])
-        <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
-        <div class="col-md-9"><span><i class="fa fa-user"></i>On-site Interview with <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
-    @elseif($placementStatus->status == PlacementStatus::statuses()[6])
-        <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
-        <div class="col-md-9"><span><i class="fa fa-thumbs-up"></i>Offer Acceptance Deadline from <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
+    @if(Auth::user()->role == "Fellow")
+        @if($placementStatus->status == PlacementStatus::statuses()[2])
+        	<div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-mobile"></i>Phone Interview with <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
+        @elseif($placementStatus->status == PlacementStatus::statuses()[4])
+            <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-user"></i>On-site Interview with <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
+        @elseif($placementStatus->status == PlacementStatus::statuses()[6])
+            <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-thumbs-up"></i>Offer Acceptance Deadline from <a href="{{ URL::to('/companies/' . $placementStatus->opportunity->company->id) }}">{{ $placementStatus->opportunity->company->name }}</a></span></div>
+        @endif
+    @elseif(Auth::user()->role == "Hiring Manager")
+        @if($placementStatus->status == PlacementStatus::statuses()[2])
+            <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-mobile"></i>Phone Interview with <a href="{{ URL::to('/fellows/' . $placementStatus->fellow->id) }}">{{ $placementStatus->fellow->user->firstName . " " . $placementStatus->fellow->user->lastName }}</a></span></div>
+        @elseif($placementStatus->status == PlacementStatus::statuses()[4])
+            <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-user"></i>On-site Interview with <a href="{{ URL::to('/fellows/' . $placementStatus->fellow->id) }}">{{ $placementStatus->fellow->user->firstName . " " . $placementStatus->fellow->user->lastName }}</a></span></div>
+        @elseif($placementStatus->status == PlacementStatus::statuses()[6])
+            <div class="col-md-3"><strong><span class="date"> {{ Carbon::createFromFormat('Y-m-d H:i:s', $placementStatus->eventDate)->diffForHumans() }}</span></strong></div>
+            <div class="col-md-9"><span><i class="fa fa-thumbs-up"></i>Offer Acceptance Deadline for <a href="{{ URL::to('/fellows/' . $placementStatus->fellow->id) }}">{{ $placementStatus->fellow->user->firstName . " " . $placementStatus->fellow->user->lastName }}</a></span></div>
+        @endif
     @endif
 </div>
