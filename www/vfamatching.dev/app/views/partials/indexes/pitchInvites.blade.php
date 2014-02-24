b4a93ee411bee71a66db0011a0118cc571f68e49
Scott Lowe <me@scottdlowe.com>
2014-02-23 22:29:13 -0500

    Removing Pitch Invite heading on fellowdashboard

--- www/vfamatching.dev/app/views/partials/indexes/pitchInvites.blade.php
+++ www/vfamatching.dev/app/views/partials/indexes/pitchInvites.blade.php
@@ -1,7 +1,9 @@
 {{-- Requires $fellow --}}
+@if($fellow->pitchInvites()->whereNull('pitch_id')->count())
 <div class="pitchInvites">
 	<h2><small>Invites to Pitch Opportunities</small></h2>
 	@foreach($fellow->pitchInvites()->whereNull('pitch_id')->get() as $pitchInvite)
 		@include("partials.indexes.pitchInvite", array("pitchInvite" => $pitchInvite))
 	@endforeach
 </div>
+@endif
