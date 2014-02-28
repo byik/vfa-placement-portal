922860176e5b51df846e8ef4840e040131e08f96
Scott Lowe <me@scottdlowe.com>
2014-02-27 23:14:53 -0500

    Removing custom report link

--- www/vfamatching.dev/app/views/reports/index.blade.php
+++ www/vfamatching.dev/app/views/reports/index.blade.php
@@ -7,10 +7,9 @@
 @section('content')
 	<div class="container">
 		<div class="row">
-			<div class="col-md-3"><a href="{{ URL::to('reports') . '/fellows' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Unplaced Fellows</a></div>
-			<div class="col-md-3"><a href="{{ URL::to('reports') . '/companies' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Unfilled Opportunities</a></div>
-			<div class="col-md-3"><a href="{{ URL::to('reports') . '/placementStatuses' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Recent Placement Status Updates</a></div>
-			<div class="col-md-3"><a href="{{ URL::to('reports') . '/custom' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Custom</a></div>
+			<div class="col-md-4"><a href="{{ URL::to('reports') . '/fellows' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Unplaced Fellows</a></div>
+			<div class="col-md-4"><a href="{{ URL::to('reports') . '/companies' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Unfilled Opportunities</a></div>
+			<div class="col-md-4"><a href="{{ URL::to('reports') . '/placementStatuses' }}" class="btn btn-primary form-control"><i class="fa fa-tachometer"></i> Recent Placement Status Updates</a></div>
 		</div>
 	</div>
 @stop
