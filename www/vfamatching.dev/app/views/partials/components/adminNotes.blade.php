860e4aa72585569b9ffb0f9ac23deda35440e3d3
Scott Lowe <me@scottdlowe.com>
2014-02-27 23:09:19 -0500

    [bugfix] Displaying adminNote box on fellow profile when no adminNotes exist

--- www/vfamatching.dev/app/views/partials/components/adminNotes.blade.php
+++ www/vfamatching.dev/app/views/partials/components/adminNotes.blade.php
@@ -1,14 +1,14 @@
 {{-- Requires $adminNotes--}}
 <div id="notes">
     <div class="container">
-        @if(count($adminNotes))
-            <div class="col-xs-12 well">
+        <div class="col-xs-12 well">
+            @if(count($adminNotes))
                 <h3>Admin Notes</h3>
                 @foreach( $adminNotes as $adminNote)
                     @include('partials.indexes.adminNote', array('adminNote' => $adminNote))
                 @endforeach
-                @include('partials.forms.adminNote', array('entityId' => $entityId, 'entityType' => $entityType))
-            </div>
-        @endif
+            @endif
+            @include('partials.forms.adminNote', array('entityId' => $entityId, 'entityType' => $entityType))
+        </div>
     </div>
 </div>
