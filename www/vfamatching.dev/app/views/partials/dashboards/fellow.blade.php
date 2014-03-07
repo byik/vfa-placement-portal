69e7e5a7b690bbdf3a9c4c5752eaf2fb1ad8d759
Scott Lowe <me@scottdlowe.com>
2014-03-06 22:34:56 -0500

    Correcting typos on Fellow Dashboard

--- www/vfamatching.dev/app/views/partials/dashboards/fellow.blade.php
+++ www/vfamatching.dev/app/views/partials/dashboards/fellow.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="pull-right">
-                    Looking for resources or wisdom from past fellows? Check out <a href="http://vfafellows.org/">vfafellow.org</a>
+                    Looking for resources or wisdom from past fellows? Check out <a href="http://vfafellows.org/">vfafellows.org</a>
                 </div>
             </div>
         </div>
@@ -27,7 +27,7 @@
                 <div class="col-xs-12">
                     <h4>Welcome to the Fellow Dashboard!</h4>
                     <p>This is where you'll find your upcoming events and progress with each Opportunity you're interested in. But you haven't been introduced to any companies yet... <p>
-                    <p>Head on over to the list of Opportunities and start pitching! Your pitches will be reviewed by VFA Staff, and you'll receive an email when they're approved.</p>
+                    <p>Head on over to the list of Opportunities and start pitching! Your pitches will be reviewed by VFA team members, and you'll receive an email when they're approved.</p>
                 </div>
             </div>
             <div class="row">
